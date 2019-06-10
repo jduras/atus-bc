e5ab46c6a3355ca75ef1266f1ff53f57905132a2
jduras <jan.duras@gmail.com>
2019-06-10 22:36:02 +0200

    change from while to map

--- code/c02_merge_atus_data.R
+++ code/c02_merge_atus_data.R
@@ -1,32 +1,19 @@
 
-print(paste("Merging data from ATUS", tfirst, "to", tlast))
+message(str_c("Merging data from ATUS ", tfst, " to ", tlst))
 
-df_timeuse_all <- NULL
-df_location_all <- NULL
+df_timeuse_all <-
+    map_dfr(tfirst:tlast,
+            function(t = .) {
+                load(str_c(edir_atus, "atus_", t, "_timeuse.Rdata"))
+                df_timeuse
+            })
 
-t <- tfirst
+df_location_all <-
+    map_dfr(tfirst:tlast,
+        function(t = .) {
+            load(str_c(edir_atus, "atus_", t, "_location.Rdata"))
+            df_location
+        })
 
-i <- 1
-
-while (t <= tlast) {
-
-    print(paste(" year", t))
-
-    load(paste0(edir_atus, "subset_", t, ".Rdata"))
-
-    df_timeuse_all[[i]] <- df_timeuse
-    df_location_all[[i]] <- df_location
-
-    rm(df_timeuse, df_location)
-
-    i <- i + 1
-    t <- t + 1
-    }
-
-df_timeuse_all %<>%  bind_rows()
-df_location_all %<>% bind_rows()
-
-save(df_timeuse_all, file = paste0(edir_atus, "atus_", tfirst, "_", tlast, "_timeuse.Rdata"))
-save(df_location_all, file = paste0(edir_atus, "atus_", tfirst, "_", tlast, "_location.Rdata"))
-
-
+save(df_timeuse_all, file = str_c(edir_atus, "atus_", tfst, "_", tlst, "_timeuse.Rdata"))
+save(df_location_all, file = str_c(edir_atus, "atus_", tfst, "_", tlst, "_location.Rdata"))
