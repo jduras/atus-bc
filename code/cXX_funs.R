5fbeaad3dfa2672db4ef9b3eee892c30fa6630ac
jduras <jan.duras@gmail.com>
2019-06-27 22:40:37 +0200

    faster calculation of clustered errors

--- code/cXX_funs.R
+++ code/cXX_funs.R
@@ -2,12 +2,14 @@
 clustered_se <- function(model, clustervar){
    # constructs clustered standard errors variance-covariance matrix
 
-   # library(lmtest)
-
    M <- length(unique(clustervar))        # number of clusters
    N <- length(clustervar)                # number of observations
    K <- model$rank                        # number of parameters in the model
    dfc <- (M/(M - 1)) * ((N - 1)/(N - K)) # degrees of freedom adjustment
-   uj <- apply(sandwich::estfun(model), 2, function(x) tapply(x, clustervar, sum))
+   uj <- as_tibble(cbind(clustervar, sandwich::estfun(model))) %>%
+      group_by(clustervar) %>%
+      summarise_all(sum) %>%
+      select(-clustervar) %>%
+      as.matrix()
    rcse.cov <- dfc * sandwich::sandwich(model, meat = crossprod(uj)/N) # clustered standard errors variance-covariance matrix
 }
